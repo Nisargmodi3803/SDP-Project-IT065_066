534a2955fcc303d833aa5392041fae6434254896
Nisarg Modi <124357940+Nisargmodi3803@users.noreply.github.com>
2024-03-09 00:03:18 +0530

    NavBar design

--- Student/Student_Frontend/src/components/VideoPlayerPage.jsx
+++ Student/Student_Frontend/src/components/VideoPlayerPage.jsx
@@ -1,21 +1,15 @@
-// VideoPlayerPage.js
 import React from 'react';
 
 const VideoPlayerPage = ({ videoId }) => {
     return (
-        <div>
-            <h2>Video Player</h2>
-            <div>
-                <iframe
-                    width="560"
-                    height="315"
-                    src={`https://www.youtube.com/embed/${videoId}`}
-                    title="YouTube Video Player"
-                    frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen
-                ></iframe>
-            </div>
+        <div style={{ width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <iframe
+                width="100%" height="100%" 
+                src={`https://www.youtube.com/embed/${videoId}`}
+                title="YouTube Video Player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen
+            ></iframe>
         </div>
     );
 };
--- Student/Student_Frontend/src/components/nav.jsx
+++ Student/Student_Frontend/src/components/nav.jsx
@@ -1,44 +1,46 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import './nav.css'
+import React from 'react';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+import './nav.css';
+import logoImage from '../assets/eLearning.webp'; // Import the logo image
 
-export default function Nav() {
-  const auth = localStorage.getItem('student')
-  const navigate = useNavigate()
+const Nav = () => {
+  const isLoggedIn = !!localStorage.getItem('student');
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
 
   const logout = () => {
     localStorage.clear();
-    navigate('/login')
-  }
+    navigate('/login');
+  };
+
   return (
-    <div>
-
-      {
-        auth ?
-          <ul className='nav-UI'>
-            <li><Link to='/home'>Home</Link></li>
-            <li><Link to='/allcourses'>All courses</Link></li>
-            <li><Link to='/searchCourse'>Search Course</Link></li>
-            <li><Link to='/enrollments'>Enrollments</Link></li>
-            <li><Link to='/notes'>Notes</Link></li>
-            <li><Link to='/profile'>Profile</Link></li>
-            <li><Link onClick={logout} to='/login'>Logout</Link></li>
-            {/* <li><div class="form-check form-switch">
-              <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-              <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
-            </div></li> */}
-          </ul>
-          :
-          <ul className='nav-UI'>
-            <li><Link to='/login'>Log in</Link></li>
-            <li><Link to='/'>Sign up</Link></li>
-            {/* <li><div className="form-check form-switch">
-              <input className="form-check-input" type='checkbox' role='switch' id="flexSwitchCheckDefault" />
-              <label className="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
-            </div></li> */}
-          </ul>
-      }
-
-    </div>
-  )
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+          <img src={logoImage} alt="Logo" className="logo-image" /> {/* Use the logo image */}
+          <span className="website-name">Learner's</span> {/* Include the website name */}
+        </div>
+        <ul className="nav-menu">
+          {isLoggedIn ? (
+            <>
+              <li><NavLink to='/home' activeClassName="active">Home</NavLink></li>
+              <li><NavLink to='/allcourses' activeClassName="active">All Courses</NavLink></li>
+              <li><NavLink to='/searchCourse' activeClassName="active">Search Course</NavLink></li>
+              <li><NavLink to='/enrollments' activeClassName="active">Enrollments</NavLink></li>
+              <li><NavLink to='/notes' activeClassName="active">Notes</NavLink></li>
+              <li><NavLink to='/profile' activeClassName="active">Profile</NavLink></li>
+              <li><button onClick={logout}>Logout</button></li>
+            </>
+          ) : (
+            <>
+              <li><NavLink to='/login' activeClassName="active">Log in</NavLink></li>
+              <li><NavLink to='/signup' activeClassName="active">Sign up</NavLink></li>
+            </>
+          )}
+        </ul>
+      </div>
+    </nav>
+  );
 }
+
+export default Nav;
