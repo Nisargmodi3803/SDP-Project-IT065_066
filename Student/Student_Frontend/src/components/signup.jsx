40122a864417bc9be61aa0f8a4e5a656245d80ec
Nisarg Modi <124357940+Nisargmodi3803@users.noreply.github.com>
2024-03-31 19:35:25 +0530

    signInWithGoogle

--- Student/Student_Frontend/src/components/signup.jsx
+++ Student/Student_Frontend/src/components/signup.jsx
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from 'react'
 import './signup.css'
 import { useNavigate } from 'react-router-dom'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { app } from './firebase';
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
 export default function Signup() {
     const [email, setEmail] = useState("")
@@ -25,7 +30,7 @@ export default function Signup() {
         if (password.match(reEnterPassword)) {
             let result = await fetch('https://sdp-project-it065-066-9.onrender.com/register', {
                 method: 'post',
-                body: JSON.stringify({ email, password,name, phoneNo, city, state }),
+                body: JSON.stringify({ email, password, name, phoneNo, city, state }),
                 headers: {
                     'content-type': 'application/json'
                 },
@@ -44,53 +49,76 @@ export default function Signup() {
         }
     }
 
+    const signInWithGoogle = async () => {
+        signInWithPopup(auth, provider).then((data) => {
+            setEmail(data.user.email);
+        });
+
+        let result = await fetch('https://sdp-project-it065-066-9.onrender.com/register', {
+            method: 'post',
+            body: JSON.stringify({ email }),
+            headers: {
+                'content-type': 'application/json'
+            },
+        });
+
+        result = await result.json()
+        console.log(result)
+
+        localStorage.setItem("student", JSON.stringify(result.result))
+        localStorage.setItem("token", JSON.stringify(result.auth))
+        navigate('/home')
+    }
+
     return (
         <div className='container-signup'>
-        <div className='wrapper'>
-        <div className='registerPage'>
-            
-            <form onSubmit={(e) => e.preventDefault}>
-            <h1>Crack Your Goal with India's top educator</h1>
-                <input className='inputBox'
-                    type='email'
-                    placeholder='Enter your Email'
-                    value={email} onChange={(e) => setEmail(e.target.value)} /><br></br>
+            <div className='wrapper'>
+                <div className='registerPage'>
 
-                <input className='inputBox'
-                    type='password'
-                    placeholder='Enter Password'
-                    value={password} onChange={(e) => setPassword(e.target.value)} /><br></br>
+                    <form onSubmit={(e) => e.preventDefault}>
+                        <h1>Crack Your Goal with India's top educator</h1>
+                        <input className='inputBox'
+                            type='email'
+                            placeholder='Enter your Email'
+                            value={email} onChange={(e) => setEmail(e.target.value)} /><br></br>
 
-                <input className='inputBox'
-                    type='password'
-                    placeholder='Re-enter password'
-                    value={reEnterPassword} onChange={(e) => setRePassword(e.target.value)} /><br></br>
+                        <input className='inputBox'
+                            type='password'
+                            placeholder='Enter Password'
+                            value={password} onChange={(e) => setPassword(e.target.value)} /><br></br>
 
-                <input className='inputBox'
-                    type='text'
-                    placeholder='Fullname'
-                    value={name} onChange={(e) => setName(e.target.value)} /><br></br>
+                        <input className='inputBox'
+                            type='password'
+                            placeholder='Re-enter password'
+                            value={reEnterPassword} onChange={(e) => setRePassword(e.target.value)} /><br></br>
 
-                <input className='inputBox'
-                    type='text'
-                    placeholder='Phone No.'
-                    value={phoneNo} onChange={(e) => setPhoneNo(e.target.value)} /><br></br>
+                        <input className='inputBox'
+                            type='text'
+                            placeholder='Fullname'
+                            value={name} onChange={(e) => setName(e.target.value)} /><br></br>
 
-                <input className='inputBox'
-                    type='text'
-                    placeholder='City'
-                    value={city} onChange={(e) => setCity(e.target.value)} /><br></br>
+                        <input className='inputBox'
+                            type='text'
+                            placeholder='Phone No.'
+                            value={phoneNo} onChange={(e) => setPhoneNo(e.target.value)} /><br></br>
 
-                <input className='inputBox'
-                    type='text'
-                    placeholder='State'
-                    value={state} onChange={(e) => setState(e.target.value)} /><br></br>
+                        <input className='inputBox'
+                            type='text'
+                            placeholder='City'
+                            value={city} onChange={(e) => setCity(e.target.value)} /><br></br>
 
-                <button onClick={collectData}
-                    type='button'>Sign up</button>
-            </form>
+                        <input className='inputBox'
+                            type='text'
+                            placeholder='State'
+                            value={state} onChange={(e) => setState(e.target.value)} /><br></br>
+
+                        <button onClick={collectData}
+                            type='button'>Sign up</button>
+                        <button onClick={signInWithGoogle} type='button'>SignIn with Google</button>
+                    </form>
+
+                </div>
+            </div>
         </div>
-    </div>
-    </div>
     )
 }   
